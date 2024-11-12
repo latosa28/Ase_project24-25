e50fa346bc069b20cd821d72442f2a0f506e0950
SSetzu <simone94h@hotmail.it>
2024-11-12 15:57:24 +0100

    add account and fix init.sql

--- src/collection/db/init.sql
+++ src/collection/db/init.sql
@@ -4,9 +4,11 @@ USE collection_db;  -- Select the database to use
 
 -- Create the item table if it does not already exist
 CREATE TABLE IF NOT EXISTS item (
-    Id_item INT AUTO_INCREMENT PRIMARY KEY,          -- Unique ID for each item, auto-incremented
-    Rarity VARCHAR(50) NOT NULL,                      -- Rarity of the item, cannot be null
-    Characteristics TEXT NOT NULL                     -- Characteristics of the item, cannot be null
+    id_item INT AUTO_INCREMENT PRIMARY KEY,          -- Unique ID for each item, auto-incremented
+    rarity DECIMAL(5,2) NOT NULL CHECK (rarity >= 0 AND rarity <= 100),
+    name VARCHAR(100) NOT NULL,                           -- Name of the item, cannot be null
+    image_path VARCHAR(255) NOT NULL,             -- URL or path to the image, cannot be null
+    UNIQUE (Name)                           -- Optional: Ensure that the item name is unique
 );
 
 -- Create the user_item table if it does not already exist
@@ -17,3 +19,7 @@ CREATE TABLE IF NOT EXISTS user_item (
     Date_roll DATETIME NOT NULL,                      -- Date when the item was added to the collection, cannot be null
     FOREIGN KEY (Id_item) REFERENCES item(Id_item)   -- Foreign key linking to the item table
 );
+
+
+INSERT IGNORE INTO item (id_item, Name, rarity, image_path)
+VALUES (1, 'Rayquaza', 0.05, '/collection_db/items_images/image1.png');
