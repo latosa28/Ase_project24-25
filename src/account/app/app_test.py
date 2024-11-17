409de0971de2d972fb5f8b4e4f156b5b2a24c72a
Giuseppe La Tosa <g.latosa@studenti.unipi.it>
2024-11-17 18:01:03 +0100

    unit_test account

--- src/account/app/app_test.py
+++ src/account/app/app_test.py
@@ -1,10 +1,9 @@
-import app as main_app  # Importa il microservizio collection
+# account_app_test.py
+import app as main_app  # Importa il microservizio account
 from flask import Flask
-
 
 # Crea un'app di test Flask basata sull'app principale
 test_app = main_app.app
-
 
 # Funzione mock per simulare le risposte del servizio `currency`
 def mock_currency_request(user_id, amount):
@@ -13,8 +12,8 @@ def mock_currency_request(user_id, amount):
     else:
         return type('MockResponse', (object,), {"status_code": 400, "json": lambda: {"error": "Mock failure"}})()
 
-
-# Imposta la funzione mock nel file collection
+# Imposta la funzione mock nel file account
 main_app.currency_request_function = mock_currency_request
+
 
 
