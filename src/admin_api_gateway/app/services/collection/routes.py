44fad423c45cb9926277491318fb80358c000a8c
Giuseppe La Tosa <g.latosa@studenti.unipi.it>
2024-11-20 15:21:34 +0100

    admin

--- src/admin_api_gateway/app/services/collection/routes.py
+++ src/admin_api_gateway/app/services/collection/routes.py
@@ -18,31 +18,31 @@ def get_items(current_user, admin_id):
 # Route to get a specific item by ID
 @collection_bp.route('/admin/<int:admin_id>/item/<int:item_id>', methods=['GET'])
 @token_required
-def get_item_by_id(current_user, item_id):
-    response = requests.get(URL + f'/item/{item_id}')
+def get_item_by_id(current_user,admin_id, item_id):
+    response = requests.get(URL + f'/admin/{admin_id}/item/{item_id}')
     return response.json(), response.status_code
 
 
-# Route per visualizzare la collezione dell'utente
+# Route per aggiornare un item della collezione dell'utente
 @collection_bp.route('/admin/<int:admin_id>/item/<int:item_id>', methods=['POST'])
 @token_required
-def update_item(admin_id, item_id):
-    response = requests.get(f"{URL}admin/{admin_id}/item/{item_id}")
+def update_item(current_user,admin_id,item_id):
+    response = requests.get(URL + f'/admin/{admin_id}/item/{item_id}')
     return response.json(), response.status_code
 
 
-# Route per ottenere informazioni su una specifica istanza di un item della collezione dell'utente
+# Route per aggiungere un item alla collezione dell'utente
 @collection_bp.route('/admin/<int:admin_id>/item/', methods=['PUT'])
 @token_required
-def add_item(admin_id):
-    response = requests.get(f"{URL}admin/{admin_id}/item/")
+def add_item(current_user,admin_id):
+    response = requests.get(URL + f'/admin/{admin_id}/item/')
     return response.json(), response.status_code
 
 
-# Route per effettuare un "roll" di gacha
+# Route per eliminare un item dalla collezione dell'utente
 @collection_bp.route('/admin/<int:admin_id>/item/<int:item_id>', methods=['DELETE'])
 @token_required
-def delete_item(admin_id, item_id):
-    response = requests.put(f"{URL}admin/{admin_id}/item/{item_id}")
+def delete_item(current_user,admin_id, item_id):
+    response = requests.put(URL + f'/admin/{admin_id}/item/{item_id}')
     return response.json(), response.status_code
     
