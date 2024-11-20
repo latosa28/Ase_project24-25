6b1d6258d5b1da5e681e8ab30c6a09efd736f04d
Giuseppe La Tosa <g.latosa@studenti.unipi.it>
2024-11-20 19:37:40 +0100

    fix

--- src/admin_api_gateway/app/services/payment/routes.py
+++ src/admin_api_gateway/app/services/payment/routes.py
@@ -7,8 +7,8 @@ payment_bp = Blueprint('payment', __name__)
 URL = "http://payment:5007"
 
 
-@payment_bp.route("/admin/<int:admin_id>/user/<int:user_id>/currency_history", methods=["GET"])
+@payment_bp.route("/admin/user/<int:user_id>/currency_history", methods=["GET"])
 @token_required
-def get_currency_history(user, admin_id, user_id):
-    response = requests.get(f'{URL}/admin/{admin_id}/user/{user_id}/currency_history')
+def get_currency_history(current_admin_id, user_id):
+    response = requests.get(f'{URL}/admin/{current_admin_id}/user/{user_id}/currency_history')
     return response.json(), response.status_code
