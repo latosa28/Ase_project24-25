64f8f01e3dd24fcce334fe61a9f0677a70e71e4f
Giuseppe La Tosa <g.latosa@studenti.unipi.it>
2024-11-22 09:51:55 +0100

    ouath2 payment

---
+++ src/payment/app/helpers/auth.py
@@ -0,0 +1,25 @@
+import json
+import requests
+from jwt.algorithms import RSAAlgorithm
+
+AUTH_URL = "http://auth:5011"
+
+
+class AuthHelper:
+
+    @staticmethod
+    def get_jwt_public_key():
+        jwks = None
+        response = requests.get(f"{AUTH_URL}/.well-known/jwks.json")
+
+        if response.status_code == 200:
+            jwks = response.json()
+
+        if not jwks:
+            raise Exception("Errore nel recupero delle chiavi JWK")
+
+        key = jwks['keys'][0]
+        if key['kty'] != 'RSA':
+            raise ValueError("Chiave non RSA, impossibile costruire la chiave pubblica.")
+        public_key = RSAAlgorithm.from_jwk(json.dumps(key))
+        return public_key
